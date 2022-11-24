4a895aa5fc42cf4ea63f2ff7e33a68229a5636f2
akarsh <akarshagri121@gmail.com>
2022-11-25 05:21:19 +0530

    a

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,13 @@
+
 var data = JSON.parse(localStorage.getItem("dashboard_data"));
 document.getElementById("name").innerHTML = data.name;
 document.getElementById("email").textContent = data.email;
+document.getElementById("ph_no").textContent = data.ph_no;
 document.getElementById("desig").textContent = data.desig;
 document.getElementById("dep").textContent = data.dep;
+document.addEventListener('DOMContentLoaded', function () {
+    alert("Ready!");
+}, false);
+document.addEventListener('readystatechange', () => {
+    if (document.readyState == 'complete') codeAddress();
+});

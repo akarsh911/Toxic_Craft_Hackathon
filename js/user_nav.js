0c48ea533d2d16cf0feaef5e7f00c6a3e715ecd2
akarsh <akarshagri121@gmail.com>
2022-11-25 02:47:59 +0530

    comps

--- js/user_nav.js
+++ js/user_nav.js
@@ -4,7 +4,6 @@ onhashchange = (event) => {
     var target = document.getElementsByTagName("body")[0];
     var chng = document.getElementById("data_html");
     var hash = window.location.hash.substring(1);
-    alert(hash);
     if (hash == "user_street_lamp_complaint" || hash == "user_house_power_supply_complaint" || hash == "user_elec_meter_complaint" || hash == "user_new_water_connection_complaint" || hash == "user_water_connection_maintenance_complaint" || hash == "user_pest_complaint" || hash == "user_stray_complaint" || hash == "user_new_connection_complaint" || hash == "user_maintenance_complaint" || hash == "user_road_maintain_complaint" || hash == "user_pothole_complaint" || hash == "user_fire_complaint" || hash == "user_medical_complaint" || hash == "user_police_complaint" || hash == "user_meter_issues_complaint" || hash == "user_leakage_complaint" || hash == "user_new_gas_complaint" || hash == "user_general_complaint") {
         chng.innerHTML = `<iframe style="width:100%;height:100%;" src="../php/user_dashboard.php?req=app_` + hash + `"></iframe>`;
     }

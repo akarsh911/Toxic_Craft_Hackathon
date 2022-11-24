6f082991e5cc31de5e7844abdfcf8d49fabb8446
akarsh <akarshagri121@gmail.com>
2022-11-25 04:09:22 +0530

    aks

--- php/complaint_form_handler.php
+++ php/complaint_form_handler.php
@@ -1,17 +1,18 @@
 <?php
-header('P3P: CP="CAO PSA OUR"');
-session_start();
-$user_id = $_GET["user_id"];
-$dep = $_GET["dep"];
-$cat = $_GET["cat"];
-$title = $_POST["title"];
-$desc = $_POST["description"];
-$file = $_POST["file"];
-$lat = $_POST["lat"];
-$long = $_POST["long"];
-require_once("../php/complaint_register.php");
-if (new_complaint($dep, $cat, $user_id, $title, $desc, $file, $lat, $long) == true) {
-    echo "success";
-} else {
-    echo "fail";
+if (isset($_GET["user_id"])) {
+
+    $user_id = $_GET["user_id"];
+    $dep = $_GET["dep"];
+    $cat = $_GET["cat"];
+    $title = $_POST["title"];
+    $desc = $_POST["description"];
+    $file = $_POST["filename"];
+    $lat = $_POST["lat"];
+    $long = $_POST["lng"];
+    require_once("../php/complaint_register.php");
+    if (new_complaint($dep, $cat, $user_id, $title, $desc, $file, $lat, $long) == true) {
+        echo "success";
+    } else {
+        echo "fail";
+    }
 }

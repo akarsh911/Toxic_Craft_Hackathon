21faaedb9d1f584440c0a76c83b517e7a3bc3b6a
akarsh <akarshagri121@gmail.com>
2022-11-25 08:17:30 +0530

    aa

--- php/complaint.php
+++ php/complaint.php
@@ -35,87 +35,10 @@ if (!check_login()) {
         </div>
 
         <div class="data_html" id="data_html">
-
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-            <body>
-                <style>
-                .totacontainer {
-                    padding-left: 50px;
-                    padding-top: 30px;
-                }
-                </style>
-                <div class="totacontainer">
-                    <div class="w3-card-4" style="width:70%">
-                        <header class="w3-container w3-light-grey">
-                            <h3>Karan Gulati</h3>
-                        </header>
-                        <div class="w3-container">
-                            <p>1 new Notification</p>
-                            <hr>
-                            <img src="https://img.favpng.com/17/15/15/computer-icons-vector-graphics-person-portable-network-graphics-clip-art-png-favpng-Mpiqqduizneyba1MwZgLaRGeM.jpg"
-                                alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                            <p>I have a problem in electricity,lights are not working ,i am complaining from a month</p>
-                            <br>
-                        </div>
-                        <button class="w3-button w3-block w3-dark-grey">+Register Complaint</button>
-                    </div>
-                </div>
+            <?php
+            require_once()
+?>
         </div>
-        <div class="totacontainer">
-            <div class="w3-card-4" style="width:70%">
-                <header class="w3-container w3-light-grey">
-                    <h3>Akarsh Srivastava</h3>
-                </header>
-                <div class="w3-container">
-                    <p>1 new complaint</p>
-                    <hr>
-                    <img src="https://img.favpng.com/17/15/15/computer-icons-vector-graphics-person-portable-network-graphics-clip-art-png-favpng-Mpiqqduizneyba1MwZgLaRGeM.jpg"
-                        alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                    <p>I have a problem in electricity,lights are not working ,i am complaining from a month</p><br>
-                </div>
-                <button class="w3-button w3-block w3-dark-grey">+Register Complaint</button>
-            </div>
-        </div>
-    </div>
-
-    <div class="totacontainer">
-        <div class="w3-card-4" style="width:70%">
-            <header class="w3-container w3-light-grey">
-                <h3>Sarthak</h3>
-            </header>
-            <div class="w3-container">
-                <p>1 new complaint</p>
-                <hr>
-                <img src="https://img.favpng.com/17/15/15/computer-icons-vector-graphics-person-portable-network-graphics-clip-art-png-favpng-Mpiqqduizneyba1MwZgLaRGeM.jpg"
-                    alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                <p>I have a problem in electricity,lights are not working ,i am complaining from a month</p><br>
-            </div>
-            <button class="w3-button w3-block w3-dark-grey">+Register Complaint</button>
-        </div>
-    </div>
-    </div>
-    <div class="totacontainer">
-        <div class="w3-card-4" style="width:70%">
-            <header class="w3-container w3-light-grey">
-                <h3>Jay</h3>
-            </header>
-            <div class="w3-container">
-                <p>1 new complaint</p>
-                <hr>
-                <img src="https://img.favpng.com/17/15/15/computer-icons-vector-graphics-person-portable-network-graphics-clip-art-png-favpng-Mpiqqduizneyba1MwZgLaRGeM.jpg"
-                    alt="Avatar" class="w3-left w3-circle w3-margin-right" style="width:60px">
-                <p>I have a problem in electricity,lights are not working ,i am complaining from a month</p><br>
-            </div>
-            <button class="w3-button w3-block w3-dark-grey">+Register Complaint</button>
-        </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    <script src="../js/dashboard.js"></script>
 </body>
 
 </html>

be6179c1c60d6fc7c3fcb7704f78300e8c982307
Sarthak <sarthaksri017@gmail.com>
2022-11-25 08:04:35 +0530

    ss

--- php/notification.php
+++ php/notification.php
@@ -1,41 +1,4 @@
-<?php
-require_once('../php/script_check_login.php');
-if (!check_login()) {
-    header("Location: /login");
-    exit();
-}
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dasboard</title>
-    <link rel="stylesheet" href="../css/dasboard_style.css" />
-    <script src="../js/load_dashboard.js"></script>
-</head>
-
-<body>
-    <!--#include virtual="../html/dasboard_header.html" -->
-    <?php include("../html/dasboard_header.html"); ?>
-    <div class="no_head">
-        <div class="navigation_container " id="navigation_container">
-            <div class="navigation_bar">
-                <?php
-                require_once("../php/user_dashboard_data.php");
-                $user_mail = get_log_in($_COOKIE["key"]);
-                $ds = get_dashboard_type($user_mail);
-                if ($ds == 0)
-                    include("../html/user_nav.html");
-                else if ($ds == 1)
-                    include("../html/admin_nav.html"); ?>
-            </div>
-        </div>
-
-        <div class="data_html" id="data_html">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <body>
@@ -114,10 +77,3 @@ if (!check_login()) {
     </div>
   </div>
   </div>
-        </div>
-    </div>
-    </div>
-    <script src="../js/dashboard.js"></script>
-</body>
-
-</html>

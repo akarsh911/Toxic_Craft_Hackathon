b0580ac52b39ba254648893bf210d92bab9dac98
Sarthak <sarthaksri017@gmail.com>
2022-11-25 08:54:05 +0530

    ss

--- js/user_complaints.js
+++ js/user_complaints.js
@@ -1,9 +1,39 @@
-var data=localStorage.getItem("complaints");
-data=JSON.parse(data);
-if(data!=="0")
+var data1=localStorage.getItem("complaints");
+const data=JSON.parse(data1);
+if(data!=null)
 {
-    for(int i=1;i<=data.count;i++)
+    alert("");
+    for(var i=1;i<=data.count;i++)
     {
-        
+        var dt=document.getElementById("comp_table").innerHTML;
+        var x=`<tr>
+        <th>
+            `+i+`
+        </th>
+        <th>
+           `+data[i].title+`
+        </th>
+        <th>
+        `+data[i].descrip+`
+        </th>
+        <th>
+        `+data[i].user_id+`
+        </th>
+        <th>
+        `+data[i].department+`
+        </th>
+        <th>
+        `+data[i].category+`
+        </th>
+        <th>
+        `+data[i].complaint_state+`
+        </th>
+        <th>
+        `+data[i].complaint_date+`
+        </th>
+        <th>
+        `+data[i].resource_url+`
+        </th>
+    </tr>`;
     }
 }

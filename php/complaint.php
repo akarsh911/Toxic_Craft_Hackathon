0c63d798112dff5738cd0ad4ecd0660396d6e7f5
akarsh <akarshagri121@gmail.com>
2022-11-25 09:42:54 +0530

    aks

--- php/complaint.php
+++ php/complaint.php
@@ -31,9 +31,9 @@ if (!check_login()) {
                     include("../html/user_nav.html");
                 else if ($ds == 1)
                     include("../html/admin_nav.html"); ?>
+
             </div>
         </div>
-
         <div class="data_html" id="data_html">
             <?php
             require_once("../php/user_dashboard_data.php");
@@ -42,10 +42,10 @@ if (!check_login()) {
             if ($ds == 0) {
                 require_once("../php/complaint_register.php");
                 $ret = complaints_user($user_mail); ?>
-            <script>
-            localStorage.setItem('complaints', '"<?php echo $ret; ?>"');
-            </script>
-            <?php
+                < script>
+                    localStorage.setItem('complaints', '"<?php echo $ret; ?>"');
+                    </script>
+                <?php
                 include("../html/user_complaints.html");
             } else if ($ds == 1)
                 include("../html/admin_complaints.html"); ?>

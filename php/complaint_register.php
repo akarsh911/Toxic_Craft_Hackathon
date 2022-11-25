4f0e60e22f6e1645f17216f39375fc294603d56c
akarsh <akarshagri121@gmail.com>
2022-11-25 07:41:21 +0530

    kks

--- php/complaint_register.php
+++ php/complaint_register.php
@@ -198,45 +198,14 @@ function complaints_user($user_id)
     }
     $all_comps = array();
     $count = 0;
-    echo "<br>
-    <br><table>";
-    echo "<style>table, th, td {
-  border: 1px solid black;
-}</style>";
-    echo `<tr>
-    <th>department</th>
-    <th>category</th>
-     <th>title</th>
-       <th>description</th>
-       <th>assigned id</th>
-       <th>complaint date</th>
-       <th>Status</th>
-      <th>lat </th>
-      <th>lon </th>
-      
-  </tr><br>`;
     if ($result->num_rows > 0) {
 
         while ($row = $result->fetch_assoc()) {
-            echo `<tr>
-    <th>` . $row['department'] . `</th>
-    <th>` . $row['category'] . `</th>
-     <th>` . $row['title'] . `</th>
-       <th>` . $row['descrip'] . `</th>
-       <th>` . $row['assigned_id'] . `</th>
-       <th>` . $row['complaint_date'] . `</th>
-       <th>` . $row['complaint_state'] . `</th>
-      <th>` . $row['y_cord'] . `</th>
-      <th>` . $row['x_cord'] . `</th>
-      
-  </tr><br>`;
             $count++;
             $all_comps[$count] = $row;
         }
-        $myJSON = json_encode($all_comps);
-        echo "<script> localStorage.setItem('dashboard_data', `" . $myJSON . "`);</script>";
 
-        return;
+        return json_encode($all_comps);
     } else {
         return 0;
     }
